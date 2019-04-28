2f5b94b282a063545f29c3c5298749a4ed7a96b2
Ali Sawari <ali.dragon.swr@gmail.com>
2019-04-29 01:02:02 +0430

    change

--- rebase.py
+++ rebase.py
@@ -3,3 +3,10 @@ print("Hey there this is just a simple practice for git rebase")
 print("OK")
 
 
+print("this is a new feature maybe ")
+
+def MyName(name):
+  return 'Hello this is {} is talking'.format(name)
+
+
+
